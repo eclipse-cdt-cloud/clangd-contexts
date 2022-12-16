d8a77b1d91d96a5008691e80271ac77308bf1cf3
Philip Langer <planger@eclipsesource.com>
2022-12-16 09:46:54 +0100

    Update yarn.lock & webpack config
    The webpack config contains an outdated rule entry.
    In the theia generator, this webpack rule is outcommented
    so we do the same here.
    
    Contributed on behalf of STMicroelectronics.

--- examples/browser/webpack.config.js
+++ examples/browser/webpack.config.js
@@ -5,14 +5,4 @@
 // @ts-check
 const config = require('./gen-webpack.config.js');
 
-/**
- * Expose bundled modules on window.theia.moduleName namespace, e.g.
- * window['theia']['@theia/core/lib/common/uri'].
- * Such syntax can be used by external code, for instance, for testing.
- */
-config.module.rules.push({
-    test: /\.js$/,
-    loader: require.resolve('@theia/application-manager/lib/expose-loader')
-});
-
 module.exports = config;
